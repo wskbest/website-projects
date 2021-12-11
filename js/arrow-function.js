980b7cee7fc08f46c3f13782be5db702065355be
wskbest <wskhanh2001@gmail.com>
2021-12-11 23:01:05 +0800

    start the roadmap of js

---
+++ js/arrow-function.js
@@ -0,0 +1,16 @@
+b=3
+
+const calculate = (a)
+calculate(3)
+
+
+/*
+setTimeout( () => {
+    console.log('I happen sooner');
+    setTimeout( () => {
+      // deeper code
+      console.log('I happen later');
+    }, 1);
+  }, 1);
+
+*/
